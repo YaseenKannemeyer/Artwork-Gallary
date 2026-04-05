e732ae8e061740498daf738e6db8a628371c9502
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-04-05 11:42:38 +0200

    feat: enhance artwork loading and display with texture mapping

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { texture } from "three/tsl";
 
 const images = [
   "socrates.jpg",
@@ -27,6 +28,8 @@ const artists = [
   "George Seurat",
 ];
 
+const textureLoader = new THREE.TextureLoader();
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
@@ -45,20 +48,30 @@ scene.add(rootNode);
 
 let count = 6;
 for (let i = 0; i < count; i++) {
+  const texture = textureLoader.load(images[i]);
+  texture.colorSpace = THREE.SRGBColorSpace;
+
   const baseNode = new THREE.Object3D();
   baseNode.rotation.y = i * ((Math.PI * 2) / count);
   rootNode.add(baseNode);
 
+  const border = new THREE.Mesh(
+    new THREE.BoxGeometry(3.2, 2.2, 0.9),
+    new THREE.MeshBasicMaterial({ color: 0x202020 }),
+  );
+  border.position.z = -4.5;
+  baseNode.add(border);
+
   const artwork = new THREE.Mesh(
     new THREE.BoxGeometry(3, 2, 0.1),
-    new THREE.MeshBasicMaterial({ color: 0xf08080 }),
+    new THREE.MeshBasicMaterial({ map: texture }),
   );
   artwork.position.z = -4;
   baseNode.add(artwork);
 }
 
 function animate() {
-  rootNode.rotation.y += 0.005;
+  rootNode.rotation.y += 0.002;
   renderer.render(scene, camera);
 }
 
